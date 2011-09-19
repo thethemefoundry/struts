2f321478247ffb5b7a735366691e3576fbd2c786
Andy Adams <aadams@jestro.com>
2011-09-19 14:49:52 -0700

    Cleaning up Select box HTML

--- classes/settings/option/select.php
+++ classes/settings/option/select.php
@@ -2,17 +2,21 @@
 
 class Settings_Option_Select extends Settings_Option {
 	public function to_html() {
+		// ID takes the form 'parentname-optionname'
 		$id = $this->parent_name() . '-' . $this->name();
+		// Name takes the form 'parentname[optionname]'
 		$name = $this->parent_name() . '[' . $this->name() . ']';
-		$selected = selected( $this->value(), $value, false );
 
 		$output = "<select id='$id' name='$name'>";
 
 		foreach ( $this->valid_values() as $value => $text ) {
+			// If this value is selected, mark it so
+			$selected = selected( $this->value(), $value, false );
+
 			$output .= "<option value='$value' $selected >$text</option>";
 		}
 
-		$output .= '</select>';
+		$output .= "</select>";
 
 		echo $output;
 	}
